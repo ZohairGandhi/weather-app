19bc3b8a1e98210dc0fe58d0914d9449b6cbe696
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2025-06-12 22:23:42 +1000

    feat: add func to get data for user inputted location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './style.css';
+import WeatherData from './modules/WeatherData';
+import UI from './modules/UI';
+
+const form = document.querySelector('#search-bar');
+const searchBar = document.querySelector('#search-bar input');
+
+form.addEventListener('submit', () => {
+  WeatherData.getWeatherJson(searchBar.value)
+    .then((dataJson) => {
+      UI.renderWeatherData({
+        address: WeatherData.getAddress(dataJson),
+        currConditions: WeatherData.getCurrentConditions(dataJson),
+        todayForecast: WeatherData.getTodayForecast(dataJson),
+        hourlyForecast: WeatherData.getTodayHourlyForecast(dataJson),
+        weeklyForecast: WeatherData.getNextWeekForecast(dataJson),
+      });
+    })
+    .catch(() => UI.renderErrorMsg());
+});
