244cd19d250855529f432ada7d5c715269815c20
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2025-06-12 17:49:16 +1000

    refactor: change else-if to if statements in formatWinDir func

--- src/modules/DataFormatter.js
+++ src/modules/DataFormatter.js
@@ -2,20 +2,26 @@ export default class DataFormatter {
   static formatWindDir(windDir) {
     if ((windDir >= 0 && windDir <= 22) || (windDir >= 338 && windDir <= 360)) {
       return 'N';
-    } else if (windDir >= 23 && windDir <= 67) {
-      return 'NE';
-    } else if (windDir >= 68 && windDir <= 112) {
-      return 'E';
-    } else if (windDir >= 113 && windDir <= 157) {
-      return 'SE';
-    } else if (windDir >= 158 && windDir <= 202) {
-      return 'S';
-    } else if (windDir >= 203 && windDir <= 247) {
-      return 'SW';
-    } else if (windDir >= 248 && windDir <= 292) {
-      return 'W';
-    } else {
-      return 'NW';
     }
+    if (windDir >= 23 && windDir <= 67) {
+      return 'NE';
+    }
+    if (windDir >= 68 && windDir <= 112) {
+      return 'E';
+    }
+    if (windDir >= 113 && windDir <= 157) {
+      return 'SE';
+    }
+    if (windDir >= 158 && windDir <= 202) {
+      return 'S';
+    }
+    if (windDir >= 203 && windDir <= 247) {
+      return 'SW';
+    }
+    if (windDir >= 248 && windDir <= 292) {
+      return 'W';
+    }
+    return 'NW';
+  }
   }
 }
